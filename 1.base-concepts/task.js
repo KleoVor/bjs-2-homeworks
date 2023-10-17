a6de165fee21654e5f119102fa1f39edb675fe90
KleoVor <t9671253993@gmail.com>
2023-10-17 12:31:08 +0300

    Домашнее задание к лекции 'Основные понятия'

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  const arr = [];
+  const d = (b * b - 4 * a * c);
+  if (d > 0) {
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  } else if (d === 0) {
+    const x = -b / (2 * a);
+    arr.push(x);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  // %% ставка в месяц = проценты / 100 / 12
+  const percentMonth = percent / 100 / 12;
+
+  // тело кредита после первоначального взноса
+  const body = (amount - contribution);
+
+  //ежемесячный взнос по формуле S * (P + (P / (((1 + P)^n) - 1)))
+  const contributionMonth = (body * (percentMonth + (percentMonth / (((Math.pow((1 + percentMonth), countMonths)) - 1)))));
+
+  // считаем сумму, которую внесет клиент по кредиту (не учитывая первоначальный взнос!!!!) - на погашение тела крелита + проценты, 
+  //округляем до 2 знаков и приводим к числовому значению
+  return +((contributionMonth * countMonths)).toFixed(2);
+
 }
