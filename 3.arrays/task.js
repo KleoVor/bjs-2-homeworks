8539b3b6e864aa5eee623cbb6ea1e3da8a6446d3
KleoVor <t9671253993@gmail.com>
2023-10-21 20:52:06 +0300

    HomeWork 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,27 @@
 function compareArrays(arr1, arr2) {
-  
+
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+    return arr1.every((element, index) => element === arr2[index]);
+
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+    //Используйте метод filter для получения нужных пользователей.
+    let arrGender = users.filter(users => users.gender === gender);
+
+    //если нет выбранного пола в массиве 
+    if (arrGender.length === 0) {
+        return 0;
+    }
+
+    //считаем сумму всех возвратов из массива по гендеру
+    const totalAge = arrGender.reduce((acc, user) => user.age + acc, 0);
+
+    //считаем средний возвраст
+    const averageAge = totalAge / arrGender.length;
+    return averageAge;
 }
